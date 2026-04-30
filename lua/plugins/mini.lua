743a2eaba033fff94550983722edd1cdd0d602e0
Yohannes <y.berhane56@gmail.com>
2026-04-30 02:56:05 -0400

    changes

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,7 +7,13 @@ return {
       --  - va)  - [V]isually select [A]round [)]parenthenses
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup {
+        mappings = {
+          around_next = 'aa',
+          inside_next = 'ii',
+        },
+        n_lines = 500,
+      }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
